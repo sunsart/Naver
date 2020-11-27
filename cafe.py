d57a421c0ba03e98dcf42dbde89a6f511c0be290
sunsart <73923217+sunsart@users.noreply.github.com>
2020-11-27 16:09:18 +0900

    Update cafe.py

--- cafe.py
+++ cafe.py
@@ -5,20 +5,32 @@ from bs4 import BeautifulSoup as bs
 import requests
 
 try:
-    #네이버카페 검색 / 키워드:김포 / 영역:제목 / 등록기간:1일 
+    #네이버카페 검색 / 키워드:김포
     driver = webdriver.Chrome()
-    url = 'https://cafe.naver.com/ca-fe/home/search/articles?q=%EA%B9%80%ED%8F%AC&se=1&pr=1'
+    url = 'https://cafe.naver.com/ca-fe/home/search/articles?q=%EA%B9%80%ED%8F%AC'
     driver.get(url)
     driver.implicitly_wait(3)
+    
+    #영역:제목 / 등록기간:1일 
+    driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[3]/div/div[1]/div[1]/ul/li[2]/div').click()
+    driver.find_element_by_xpath('//*[@id="app"]/div/div[2]/div/div[1]/div[3]/div/div[1]/div[2]/ul/li[2]/div').click()
 
+    #
     html = driver.page_source
     soup = bs(html, 'html.parser')
 
-    list = soup.select('.article_item')
-    print(list[0])
+    #
+    list = soup.select('.item_subject')
+    for a in list:
+        req = requests.get(a['href'])
+        text = req.text
+        soup2 = bs(text, 'html.parser')
 
-    #for article in articles:
-    #    print(article.get_text())
+
+        div = soup2.find('div', class_='profile_info')
+        print(div)
+        #a = div.find('a', class_='nickname')
+        #print(a.text)
  
 
 except Exception as e:
